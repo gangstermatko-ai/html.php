507e0459b4962466c579e4b1879e22502be3a647
gangstermatko-ai <gangstermatko@gmail.com>
2026-05-11 20:56:41 +0200

    Update dbhinc.php

--- kross/includes/dbhinc.php
+++ kross/includes/dbhinc.php
@@ -11,3 +11,18 @@ try {
     echo "zle pripojenie: ". $e->getMessage();   
 }
 
+//vraj lepsi tento dole ale je to to iste
+<?php
+
+$host = 'localhost';
+$dbname = 'myfirstdatabase';
+$dbusername = 'root';
+$dbpassword = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, 
+    $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
